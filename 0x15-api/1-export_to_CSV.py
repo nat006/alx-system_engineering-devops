41c938b81c4ed12047b5620f57b8cb2264b5560b
nat006 <nthatipmoloi006@gmail.com>
2024-03-25 08:52:45 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,17 +15,17 @@ if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com/"
 
     # Fetch employee details from the API and convert the response to JSON
-    user = requests.get(url + "users/{}".format(user_id)).json()
+    employee_id = requests.get(url + "users/{}".format(employee_id)).json()
 
     # Extract the username from the fetched employee data
     username = employee_data.get("username")
 
     # Fetch the to-do list items associated with the employee ID
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
     # Create a CSV file for exporting the to-do list information
     with open(f"{employee_id}.csv", "w", newline="") as csv_file:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
+            [employee_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
