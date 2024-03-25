c18dfd423423cc68fa5311c692cccc9a0b08e50e
nat006 <nthatipmoloi006@gmail.com>
2024-03-25 09:00:48 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,23 +9,23 @@ import sys
 
 if __name__ == "__main__":
     # Get the employee ID from the command-line arguments
-    employee_id = sys.argv[1]
+    user_id = sys.argv[1]
 
     # Base URL for the JSON API
-    base_url = "https://jsonplaceholder.typicode.com/"
+    url = "https://jsonplaceholder.typicode.com/"
 
     # Fetch employee details from the API and convert the response to JSON
-    employee_id = requests.get(url + "users/{}".format(employee_id)).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
-    # Extract the username from the fetched employee data
-    username = employee_data.get("username")
+    # Extract the username from the user data
+    username = user.get("username")
 
     # Fetch the to-do list items associated with the employee ID
-    todos = requests.get(url + "todos", params={"userId": employee_id}).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
     # Create a CSV file for exporting the to-do list information
-    with open(f"{employee_id}.csv", "w", newline="") as csv_file:
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
-            [employee_id, username, t.get("completed"), t.get("title")]
+            [user_id, username, t.get("completed"), t.get("title")]
          ) for t in todos]
