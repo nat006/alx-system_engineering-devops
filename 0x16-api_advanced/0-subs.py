a006bbe57c24093e614e890f6d51bbdc2f343423
nat006 <nthatipmoloi006@gmail.com>
2024-04-09 09:28:12 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Module documentation: my_module
+
+Provides func to query the Reddit API and return the no. of subs for given sub
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Get the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: Number of subs of subreddit. Returns 0 if subreddit is invalid.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
