7b9131b7a790a52fa2bad1a750a7aa147a27ae67
nat006 <nthatipmoloi006@gmail.com>
2024-04-09 10:44:34 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Script that queries subscribers on a given Reddit subreddit.
 """
@@ -10,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Linux:0x16 api.advanced:v1.0.0 (by u/nm01)'}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
